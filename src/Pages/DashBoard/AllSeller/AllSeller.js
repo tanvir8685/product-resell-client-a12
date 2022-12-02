079c3e94b732d4fa93cc20d88905140ad265aef6
tanvir <tanvirhridoy1011@gmail.com>
2022-12-02 14:11:50 +0600

    admin can delete seller and buyrs

--- src/Pages/DashBoard/AllSeller/AllSeller.js
+++ src/Pages/DashBoard/AllSeller/AllSeller.js
@@ -1,17 +1,38 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
+import Loading from '../../Shared/Loading/Loading';
 
 const AllSeller = () => {
-    const { data: sellers = [] } = useQuery({
+    const { data: sellers = [], refetch,isLoading} = useQuery({
         queryKey: ['user'],
         queryFn: async () => {
             const res = await fetch('http://localhost:5000/alluser?categori=seller');
             const data = await res.json();
             return data;
         }
+    })
+    if (isLoading){
+        return <Loading></Loading>
     }
 
-    )
+    const handleDeleteSeller=id=>{
+        fetch(`http://localhost:5000/alluser/${id}`,{
+            method:'DELETE',
+            headers:{
+                authorization:`bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            if(data.deletedCount>0){
+                refetch()
+                toast.success('deleted successfully')
+            }
+        })
+    }
+
+
     return (
         <div>
             <h2>This is all sell er</h2>
@@ -31,10 +52,10 @@ const AllSeller = () => {
                         
                         {
                             sellers.map(seller=><tr key={seller._id}>
-                                <td><button className='btn btn-primary'>Delete</button></td>
+                                <td><button onClick={()=>handleDeleteSeller(seller._id)} className='btn btn-primary'>Delete</button></td>
                                 <td>{seller.name}</td>
                                 <td>{seller.email}</td>
-                                <td><button className='btn btn-primary'>Make Admin</button></td>
+                                {/* <td><button className='btn btn-primary'>Make Admin</button></td> */}
                                 
                             </tr>)
                         }
